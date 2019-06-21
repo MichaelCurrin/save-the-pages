b9e82058e9d9387239da092e63f0266a961c2cd7
Michael Currin <michael.currin@gmail.com>
2019-06-21 21:54:10 +0200

    feature: Update models.py

--- savethepages/models.py
+++ savethepages/models.py
@@ -1,15 +1,20 @@
+"""
+Models module.
+"""
 import datetime
 
 import mongoengine as me
 
 
 class Label(me.Document):
-    title = me.StringField(max_length=50, unique=True, required=True)
+    name = me.StringField(max_length=50, unique=True, required=True)
 
 
 class Page(me.Document):
     title = me.StringField(max_length=200)
+    url = me.StringField(max_length=1000)
     content = me.StringField(required=True)
+    status_code = me.IntField(max_value=1000)
 
     label = me.ReferenceField(Label, required=True)
 
