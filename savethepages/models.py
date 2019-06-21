3c18354b3e242b21ad200595e6ee46a28067cd82
Michael Currin <michael.currin@gmail.com>
2019-06-21 23:19:16 +0200

    feature: Add to models.py

--- savethepages/models.py
+++ savethepages/models.py
@@ -6,6 +6,9 @@ import datetime
 import mongoengine as me
 
 
+import lib
+
+
 class Label(me.Document):
     """
     Models a label.
@@ -22,7 +25,7 @@ class Page(me.Document):
     a plain text HTML file on disk.
     """
     title = me.StringField(max_length=200)
-    url = me.StringField(max_length=1000)
+    url = me.StringField(max_length=1000, required=True)
     content = me.StringField(required=True)
     status_code = me.IntField(max_value=1000)
 
@@ -30,3 +33,20 @@ class Page(me.Document):
 
     date_added = me.DateTimeField(default=datetime.datetime.now)
     date_modified = me.DateTimeField(default=datetime.datetime.now)
+
+    def preview_url(self):
+        return lib.truncate(self.url, width=30)
+
+    def preview_content(self, width=80):
+        if self.content:
+            return lib.truncate(self.content, width)
+
+        return "(not set)"
+
+    def __str__(self):
+        """
+        Use !r here to return the repr value. For example, string quotes are
+        included and newlines are printed as escaped characters.
+        """
+        return f"<Page: title={self.title!r} status_code={self.status_code!r}"\
+               f" url={self.preview_url()!r} content={self.preview_content(20)!r}>"
