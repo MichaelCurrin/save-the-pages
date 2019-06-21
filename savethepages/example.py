acc511cf70f750a66339b38cd7cef909047655a7
Michael Currin <michael.currin@gmail.com>
2019-06-21 21:55:37 +0200

    feature: Create example.py

---
+++ savethepages/example.py
@@ -0,0 +1,43 @@
+"""
+Example application.
+
+Code used from an introductory tutorial on Python and Mongo.
+    https://realpython.com/introduction-to-mongodb-and-python/
+"""
+import datetime
+from mongoengine import *
+connect('mongoengine_test', host='localhost', port=27017)
+
+
+class Post(Document):
+    title = StringField(required=True, max_length=200)
+    content = StringField(required=True)
+    author = StringField(required=True, max_length=50)
+    published = DateTimeField(default=datetime.datetime.now)
+
+    @queryset_manager
+    def live_posts(self, queryset):
+        return queryset.filter(published=True)
+
+
+def main():
+    post_1 = Post(
+        title='Sample Post',
+        content='Some engaging content',
+        author='Scott'
+    )
+    post_1.save()       # This will perform an insert
+    print(post_1.title)
+    post_1.title = 'A Better Post Title'
+    post_1.save()       # This will perform an atomic edit on "title"
+    print(post_1.title)
+
+    post_2 = Post(content='Content goes here', author='Michael')
+    try:
+        post_2.save()
+    except ValidationError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
