65d550a28c618e82466813004edc93fc682c4d49
Michael Currin <michael.currin@gmail.com>
2019-06-24 22:01:46 +0200

    feature: Update test.py

--- savethepages/test.py
+++ savethepages/test.py
@@ -6,18 +6,40 @@ Test the basic upsert logic and the model.
 import lib
 from models import Page, Label
 
-lib.connect()
-
 
 def main():
+    import connection
+
     label = Label.objects(name='_testing').upsert_one(name='_testing')
-    post = Page(
-        title="TWO",
-        content="<div>",
+
+    post1 = Page(
+        title="Foo",
+        content="<div>Foo bar</div>",
+        url='https://foo.bar.com',
         label=label
     )
-    post.save()
-    print(post)
+    print(repr(post1))
+    post1.save()
+    print()
+
+    post2 = Page(
+        title="Bar",
+        content="<div>Fizz</div>",
+        url='https://fub.buzz.com',
+        label=label
+    )
+    print(repr(post2))
+    post2.save()
+    print()
+
+    posts = Page.objects(label=label)
+    print(posts.count())
+    print(posts)
+    print()
+
+    print("Deleting")
+    post1.delete()
+    post2.delete()
 
 
 if __name__ == "__main__":
