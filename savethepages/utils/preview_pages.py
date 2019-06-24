da7468fb90bff38587c8b612134efa89d32a24eb
Michael Currin <michael.currin@gmail.com>
2019-06-24 22:03:10 +0200

    feature: Update preview script

--- savethepages/utils/preview_pages.py
+++ savethepages/utils/preview_pages.py
@@ -1,13 +1,18 @@
+"""
+Preview module.
+
+Show contents of the DB.
+"""
 import connection
 import models
 
 
 print("Labels")
-for l in models.Label.objects():
+for l in models.Label.objects:
     print(l)
     print()
 
 print("Pages")
-for p in models.Page.objects():
+for p in models.Page.objects:
     print(p)
     print()
