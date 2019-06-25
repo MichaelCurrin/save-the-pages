02088d630d13d5915dc27769c183140f7d7c0e59
Michael Currin <michael.currin@gmail.com>
2019-06-25 21:36:19 +0200

    feature: Add to import_urls.py refactor: Update import_urls.py

--- savethepages/import_urls.py
+++ savethepages/import_urls.py
@@ -1,7 +1,66 @@
+"""
+Import URLs application file.
+"""
+from urllib import parse
+
 from mongoengine import NotUniqueError
 
 import lib
-from models import Page, Label
+from models import URL_MAX, Label, Page
+
+
+def upsert_label(name):
+    labels = Label.objects(name=name)
+    if labels.count():
+        return labels.first()
+
+    return labels.upsert_one(name=name)
+
+
+def insert_page(page_data, label):
+    """
+    Insert a page record into the DB with given data and label object.
+
+    :return: None
+    """
+    url = page_data['url']
+    # Do this here as adding a look is more involved and on save or validate
+    # might not be the right place.
+    if len(url) > URL_MAX:
+        url_data = parse.urlsplit(url)
+        url_data = url_data._replace(path='', query='')
+        url = parse.urlunsplit(url_data)
+
+    page = Page(
+        title=page_data.get('title'),
+        url=url,
+        label=label,
+    )
+    try:
+        page.save()
+    except NotUniqueError:
+        print("exists")
+    else:
+        print("added")
+
+
+def load(label_name, pages_to_import):
+    """
+    Bulk load pages into the DB with a given label.
+
+    Note that the bulk insert operation will not work in mongoengine.
+    """
+    label = upsert_label(label_name)
+    print(repr(label))
+
+    i = -1
+    for i, page_data in enumerate(pages_to_import):
+        print(i+1)
+        try:
+            insert_page(page_data, label)
+        except Exception:
+            print(page_data)
+            raise
 
 
 def main():
@@ -17,26 +76,7 @@ def main():
     file_path = f"var/{filename}"
 
     pages_to_import = lib.handle_onetab_text(file_path)
-    label_name = filename
-    label = Label.objects(name=label_name).upsert_one(name=label_name)
-
-    for page in pages_to_import:
-        url = page['url']
-        print(url)
-
-        print(page)
-        page_rec = Page(
-            title=page['title'],
-            url=url,
-            label=label,
-        )
-        try:
-            page_rec.save()
-        except NotUniqueError:
-            print("- exists")
-        else:
-            print("- added")
-        print()
+    load(filename, pages_to_import)
 
 
 if __name__ == "__main__":
